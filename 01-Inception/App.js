2fa697e0e24d640fa9b9e1e35f74e2e7f391f8b5
Ranesh <raneshsuthar64@gmail.com>
2023-01-02 16:13:31 +0530

    Day1

---
+++ 01-Inception/App.js
@@ -0,0 +1,17 @@
+ // DOM Manipulation using JavaScript
+ const heading = document.createElement("h1");
+ heading.innerHTML = "Namaste React Using JavaScript";
+ const root = document.getElementById("root");
+ root.appendChild(heading);
+
+ // DOM Manipulation using ReactDOM
+ const heading1 = React.createElement(
+   "h1",
+   { id: "title" },
+   "Heading1"
+ );
+
+ const heading2 = React.createElement("h2", {}, "Heading2");
+ const container = React.createElement("div", {}, [heading1, heading2]);
+ const reactroot = ReactDOM.createRoot(document.getElementById("reactroot"));
+ reactroot.render(container);
